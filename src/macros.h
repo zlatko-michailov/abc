df64b57ba85da8a4b22caab84ac53e1075129f56
Zlatko Michailov <zlatko@michailov.org>
2019-02-07 00:01:26 -0800

    Fix macros.

--- src/macros.h
+++ src/macros.h
@@ -5,16 +5,14 @@
 
 
 #define abc_assert_ret(cond, cat, tag, ret) { \
-	bool _cond = (cond); \
-	if (!_cond) { \
-		abc::log::diag.push(abc::severity::critical, (category), (tag), abc::status::assert_failed); \
+	if (!(cond)) { \
+		abc::log::diag.push(abc::severity::critical, (cat), (tag), abc::status::assert_failed); \
 		return (ret); \
 	} \
 }
 
 #define abc_assert_void(cond, cat, tag) { \
-	bool _cond = (cond); \
-	if (!_cond) { \
+	if (!(cond)) { \
 		abc::log::diag.push(abc::severity::critical, (cat), (tag), abc::status::assert_failed); \
 		return; \
 	} \
@@ -25,7 +23,7 @@
 
 #define abc_check(st, sev, cat, tag) { \
 	abc::status_t _st = (st); \
-	if (abc::status::succeeded(_st) { \
+	if (abc::status::succeeded(_st)) { \
 		abc::log::diag.push(abc::severity::debug, (cat), (tag), _st); \
 	} \
 	else { \
