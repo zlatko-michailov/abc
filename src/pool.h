b5ea264e56da80eb450c5588192529fc97a5c655
Zlatko Michailov <zlatko@michailov.org>
2019-03-17 16:36:00 +0200

    R2: Pool - style

--- src/pool.h
+++ src/pool.h
@@ -5,6 +5,7 @@
 
 
 namespace abc {
+
 	template <typename InstanceId>
 	class unlimited_pool;
 
@@ -15,25 +16,22 @@ namespace abc {
 	class instance;
 
 
+	// --------------------------------------------------------------
+
+
 	template <typename InstanceId>
 	class unlimited_pool {
 	public:
 		typedef InstanceId instance_id_t;
 		
 	public:
-		unlimited_pool() noexcept
-			: _instance_id(0) {
-		}
+		unlimited_pool() noexcept;
 
 	protected:
 		friend class instance<InstanceId, unlimited_pool<InstanceId>>;
 
-		InstanceId reserve_instance() noexcept {
-			return _instance_id++;
-		}
-
-		void release_instance() noexcept {
-		}
+		InstanceId reserve_instance() noexcept;
+		void release_instance() noexcept;
 
 	private:
 		std::atomic<InstanceId>	_instance_id;
@@ -43,27 +41,13 @@ namespace abc {
 	template <typename InstanceId>
 	class pool : public unlimited_pool<InstanceId> {
 	public:
-		pool(InstanceId capacity) noexcept
-			: unlimited_pool<InstanceId>()
-			, _capacity(capacity)
-			, _instance_count(0) {
-		}
+		pool(InstanceId capacity) noexcept;
 
 	protected:
 		friend class instance<InstanceId, pool<InstanceId>>;
 
-		InstanceId reserve_instance() {
-			if (++(_instance_count) > _capacity) {
-				--(_instance_count);
-				throw;
-			}
-
-			return unlimited_pool<InstanceId>::reserve_instance();
-		}
-
-		void release_instance() noexcept {
-			--(_instance_count);
-		}
+		InstanceId reserve_instance();
+		void release_instance() noexcept;
 
 	private:
 		const InstanceId		_capacity;
@@ -77,25 +61,86 @@ namespace abc {
 		typedef InstanceId instance_id_t;
 		
 	public:
-		instance(Pool& pool)
-			: _pool(pool) {
-			_instance_id = pool.reserve_instance();
-		}
+		instance(Pool& pool);
+		~instance() noexcept;
 
-		~instance() noexcept {
-			_pool.release_instance();
-		}
+		InstanceId instance_id() const noexcept;
 
-		InstanceId instance_id() const noexcept {
-			return _instance_id;
-		}
-
-		Pool& pool() noexcept {
-			return _pool;
-		}
+		Pool& pool() noexcept;
 
 	private:
 		Pool&			_pool;
 		InstanceId		_instance_id;
 	};
+
+
+	// --------------------------------------------------------------
+
+
+	template <typename InstanceId>
+	inline unlimited_pool<InstanceId>::unlimited_pool() noexcept
+		: _instance_id(0) {
+	}
+
+
+	template <typename InstanceId>
+	inline InstanceId unlimited_pool<InstanceId>::reserve_instance() noexcept {
+		return _instance_id++;
+	}
+
+
+	template <typename InstanceId>
+	inline void unlimited_pool<InstanceId>::release_instance() noexcept {
+	}
+
+
+	template <typename InstanceId>
+	inline pool<InstanceId>::pool(InstanceId capacity) noexcept
+		: unlimited_pool<InstanceId>()
+		, _capacity(capacity)
+		, _instance_count(0) {
+	}
+
+
+	template <typename InstanceId>
+	inline InstanceId pool<InstanceId>::reserve_instance() {
+		if (++(_instance_count) > _capacity) {
+			--(_instance_count);
+			throw; // TODO: What exception?
+		}
+
+		return unlimited_pool<InstanceId>::reserve_instance();
+	}
+
+
+	template <typename InstanceId>
+	inline void pool<InstanceId>::release_instance() noexcept {
+		--(_instance_count);
+	}
+
+
+	template <typename InstanceId, typename Pool>
+	inline instance<InstanceId, Pool>::instance(Pool& pool)
+		: _pool(pool) {
+		_instance_id = pool.reserve_instance();
+	}
+
+
+	template <typename InstanceId, typename Pool>
+	inline instance<InstanceId, Pool>::~instance() noexcept {
+		_pool.release_instance();
+	}
+
+
+	template <typename InstanceId, typename Pool>
+	inline InstanceId instance<InstanceId, Pool>::instance_id() const noexcept {
+		return _instance_id;
+	}
+
+
+	template <typename InstanceId, typename Pool>
+	inline Pool& instance<InstanceId, Pool>::pool() noexcept {
+		return _pool;
+	}
+
 }
