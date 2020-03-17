57a7e1e78935e2a7fe5c0fff1c659fbc2e47a6e5
Zlatko Michailov <zlatko@michailov.org>
2020-03-16 23:49:46 -0700

    Tagging: undo manual tagging.

--- test/socket.cpp
+++ test/socket.cpp
@@ -37,73 +37,73 @@ namespace abc { namespace test { namespace socket {
 		const char response_content[] = "The corresponding response content.";
 		bool passed = true;
 
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x210, "server: begin");
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: begin");
 		abc::udp_socket server;
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x211, "server: created");
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: created");
 		server.bind(server_port);
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x212, "server: bound");
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: bound");
 
 		std::thread client_thread([&passed, &context, server_port, request_content, response_content] () {
 			try {
-					context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x231, "client: begin");
+					context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "client: begin");
 				abc::udp_socket client;
-					context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x232, "client: created");
+					context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "client: created");
 				client.connect("localhost", server_port);
-					context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x233, "client: connected");
+					context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "client: connected");
 
 				std::uint16_t content_length = std::strlen(request_content);
 				client.send(&content_length, sizeof(std::uint16_t));
-					context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x234, "client: sent 2 bytes: %d", content_length);
+					context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "client: sent 2 bytes: %d", content_length);
 				client.send(request_content, content_length);
-					context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x235, "client: sent %d bytes: %s", content_length, request_content);
+					context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "client: sent %d bytes: %s", content_length, request_content);
 
-					context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x236, "client: will receive 2 bytes");
+					context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "client: will receive 2 bytes");
 				client.receive(&content_length, sizeof(std::uint16_t));
-					context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x237, "client: received 2 bytes: %d", content_length);
+					context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "client: received 2 bytes: %d", content_length);
 
 				char content[1024];
 				client.receive(content, content_length);
 				content[content_length] = '\0';
-					context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x238, "client: received %d bytes: %s", content_length, content);
+					context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "client: received %d bytes: %s", content_length, content);
 
-				passed = context.are_equal(content, response_content, 0x202) && passed;
+				passed = context.are_equal(content, response_content,__TAG__) && passed;
 			}
 			catch (const std::exception& ex) {
-				context.log.push_back(abc::category::abc::base, abc::severity::important, 0x241, "client: EXCEPTION: %s", ex.what());
+				context.log.push_back(abc::category::abc::base, abc::severity::important, __TAG__, "client: EXCEPTION: %s", ex.what());
 			}
 
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x242, "client: end");
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "client: end");
 		});
 
 		abc::socket::address client_address;
 		std::uint16_t content_length;
 		server.receive(&content_length, sizeof(std::uint16_t), &client_address);
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x213, "server: received 2 bytes: %d", content_length);
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x214, "server: client address: %d.%d.%d.%d port: %d",
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: received 2 bytes: %d", content_length);
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: client address: %d.%d.%d.%d port: %d",
 				client_address.value.sa_data[2], client_address.value.sa_data[3], client_address.value.sa_data[4], client_address.value.sa_data[5],
 				::ntohs(*(std::uint16_t*)client_address.value.sa_data));
 
 		char content[1024];
 		server.receive(content, content_length);
 		content[content_length] = '\0';
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x215, "server: received %d bytes: %s", content_length, content);
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: received %d bytes: %s", content_length, content);
 
-		passed = context.are_equal(content, request_content, 0x201) && passed;
+		passed = context.are_equal(content, request_content, __TAG__) && passed;
 
 		server.connect(client_address);
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x216, "server: connected");
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: connected");
 
 		content_length = std::strlen(response_content);
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x217, "server: will send 2 bytes: %d", content_length);
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: will send 2 bytes: %d", content_length);
 		server.send(&content_length, sizeof(std::uint16_t));
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x218, "server: sent 2 bytes: %d", content_length);
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: sent 2 bytes: %d", content_length);
 
 		server.send(response_content, content_length);
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x219, "server: sent %d bytes", content_length);
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: sent %d bytes", content_length);
 
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x220, "server: wait for client");
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: wait for client");
 		client_thread.join();
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x221, "server: end");
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: end");
 		return passed;
 	}
 
@@ -114,73 +114,73 @@ namespace abc { namespace test { namespace socket {
 		const char response_content[] = "The corresponding response content.";
 		bool passed = true;
 
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x210, "server: begin");
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: begin");
 		abc::tcp_server_socket server;
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x211, "server: created");
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: created");
 		server.bind(server_port);
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x212, "server: bound");
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x213, "server: will listen");
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: bound");
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: will listen");
 		server.listen(5);
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x214, "server: listening");
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: listening");
 
 		std::thread client_thread([&passed, &context, server_port, request_content, response_content] () {
 			try {
-					context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x231, "client: begin");
+					context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "client: begin");
 				abc::tcp_client_socket client;
-					context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x232, "client: created");
+					context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "client: created");
 				client.connect("localhost", server_port);
-					context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x233, "client: connected");
+					context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "client: connected");
 
 				std::uint16_t content_length = std::strlen(request_content);
 				client.send(&content_length, sizeof(std::uint16_t));
-					context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x234, "client: sent 2 bytes: %d", content_length);
+					context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "client: sent 2 bytes: %d", content_length);
 				client.send(request_content, content_length);
-					context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x235, "client: sent %d bytes: %s", content_length, request_content);
+					context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "client: sent %d bytes: %s", content_length, request_content);
 
-					context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x236, "client: will receive 2 bytes");
+					context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "client: will receive 2 bytes");
 				client.receive(&content_length, sizeof(std::uint16_t));
-					context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x237, "client: received 2 bytes: %d", content_length);
+					context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "client: received 2 bytes: %d", content_length);
 
 				char content[1024];
 				client.receive(content, content_length);
 				content[content_length] = '\0';
-					context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x238, "client: received %d bytes: %s", content_length, content);
+					context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "client: received %d bytes: %s", content_length, content);
 
-				passed = context.are_equal(content, response_content, 0x202) && passed;
+				passed = context.are_equal(content, response_content, __TAG__) && passed;
 			}
 			catch (const std::exception& ex) {
-				context.log.push_back(abc::category::abc::base, abc::severity::important, 0x241, "client: EXCEPTION: %s", ex.what());
+				context.log.push_back(abc::category::abc::base, abc::severity::important, __TAG__, "client: EXCEPTION: %s", ex.what());
 			}
 
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x242, "client: end");
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "client: end");
 		});
 
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x215, "server: waiting for connection");
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: waiting for connection");
 		abc::tcp_client_socket client = std::move(server.accept());
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x216, "server: accepted");
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: accepted");
 
 		std::uint16_t content_length;
 		client.receive(&content_length, sizeof(std::uint16_t));
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x217, "server: received 2 bytes: %d", content_length);
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: received 2 bytes: %d", content_length);
 
 		char content[1024];
 		client.receive(content, content_length);
 		content[content_length] = '\0';
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x218, "server: received %d bytes: %s", content_length, content);
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: received %d bytes: %s", content_length, content);
 
-		passed = context.are_equal(content, request_content, 0x201) && passed;
+		passed = context.are_equal(content, request_content, __TAG__) && passed;
 
 		content_length = std::strlen(response_content);
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x219, "server: will send 2 bytes: %d", content_length);
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: will send 2 bytes: %d", content_length);
 		client.send(&content_length, sizeof(std::uint16_t));
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x218, "server: sent 2 bytes: %d", content_length);
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: sent 2 bytes: %d", content_length);
 
 		client.send(response_content, content_length);
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x220, "server: sent %d bytes", content_length);
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: sent %d bytes", content_length);
 
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x221, "server: wait for client");
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: wait for client");
 		client_thread.join();
-			context.log.push_back(abc::category::abc::base, abc::severity::debug, 0x222, "server: end");
+			context.log.push_back(abc::category::abc::base, abc::severity::debug, __TAG__, "server: end");
 		return passed;
 	}
 
--- test/timestamp.cpp
+++ test/timestamp.cpp
@@ -78,16 +78,16 @@ namespace abc { namespace test { namespace timestamp {
 
 	static bool test_timestamp_properties(test_context<abc::test_log>& context, const abc::timestamp<>& ts, year_t year, month_t month, day_t day, hour_t hours, minute_t minutes, second_t seconds, millisecond_t milliseconds, microsecond_t microseconds, nanosecond_t nanoseconds) {
 		bool passed = true;
-		passed = context.are_equal<std::int32_t>(ts.year(),			year, 0x101, "%d") && passed;
-		passed = context.are_equal<std::int32_t>(ts.month(),		month, 0x102, "%d") && passed;
-		passed = context.are_equal<std::int32_t>(ts.day(),			day, 0x103, "%d") && passed;
+		passed = context.are_equal<std::int32_t>(ts.year(),			year, __TAG__, "%d") && passed;
+		passed = context.are_equal<std::int32_t>(ts.month(),		month, __TAG__, "%d") && passed;
+		passed = context.are_equal<std::int32_t>(ts.day(),			day, __TAG__, "%d") && passed;
 
-		passed = context.are_equal<std::int32_t>(ts.hours(),		hours, 0x104, "%d") && passed;
-		passed = context.are_equal<std::int32_t>(ts.minutes(),		minutes, 0x105, "%d") && passed;
-		passed = context.are_equal<std::int32_t>(ts.seconds(),		seconds, 0x106, "%d") && passed;
-		passed = context.are_equal<std::int32_t>(ts.milliseconds(),	milliseconds, 0x107, "%d") && passed;
-		passed = context.are_equal<std::int32_t>(ts.microseconds(),	microseconds, 0x108, "%d") && passed;
-		passed = context.are_equal<std::int32_t>(ts.nanoseconds(),	nanoseconds, 0x109, "%d") && passed;
+		passed = context.are_equal<std::int32_t>(ts.hours(),		hours, __TAG__, "%d") && passed;
+		passed = context.are_equal<std::int32_t>(ts.minutes(),		minutes, __TAG__, "%d") && passed;
+		passed = context.are_equal<std::int32_t>(ts.seconds(),		seconds, __TAG__, "%d") && passed;
+		passed = context.are_equal<std::int32_t>(ts.milliseconds(),	milliseconds, __TAG__, "%d") && passed;
+		passed = context.are_equal<std::int32_t>(ts.microseconds(),	microseconds, __TAG__, "%d") && passed;
+		passed = context.are_equal<std::int32_t>(ts.nanoseconds(),	nanoseconds, __TAG__, "%d") && passed;
 
 		return passed;
 	}
