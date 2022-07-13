0436a5393561bdbcf495f91b434430af1280593a
Zlatko Michailov <zlatko@michailov.org>
2022-07-12 21:36:48 -0700

    doxygen: socket

--- src/i/socket.i.h
+++ src/i/socket.i.h
@@ -98,67 +98,233 @@ namespace abc {
 	// --------------------------------------------------------------
 
 
+	/**
+	 * @brief					Common socket functionality. Not directly constructable.
+	 * @tparam Log				Logging facility.
+	 */
 	template <typename Log>
 	class _basic_socket {
 	protected:
+		/**
+		 * @brief				Constructor.
+		 * @param kind			Stream or datagram.
+		 * @param family		IPv4 or IPv6.
+		 * @param log			Pointer to a `Log` instance. May be `nullptr`.
+		 */
 		_basic_socket(socket::kind_t kind, socket::family_t family, Log* log);
+
+		/**
+		 * @brief				Constructor.
+		 * @param handle		Socket handle.
+		 * @param kind			Stream or datagram.
+		 * @param family		IPv4 or IPv6.
+		 * @param log			Pointer to a `Log` instance. May be `nullptr`.
+		 */
 		_basic_socket(socket::handle_t handle, socket::kind_t kind, socket::family_t family, Log* log);
+
+		/**
+		 * @brief				Move constructor.
+		 */
 		_basic_socket(_basic_socket&& other) noexcept;
+
+		/**
+		 * @brief				Deleted.
+		 */
 		_basic_socket(const _basic_socket& other) = delete;
 
+		/**
+		 * @brief				Destructor.
+		 */
 		~_basic_socket() noexcept;
 
 	public:
-		bool				is_open() const noexcept;
-		void				close() noexcept;
-		void				bind(const char* port);
-		void				bind(const char* host, const char* port);
+		/**
+		 * @brief				Returns whether the socket is open.
+		 */
+		bool is_open() const noexcept;
+
+		/**
+		 * @brief				Closes the socket.
+		 */
+		void close() noexcept;
+
+		/**
+		 * @brief				Binds the socket to the given port on all host names.
+		 * @param port			Port number (as a string).
+		 */
+		void bind(const char* port);
+
+		/**
+		 * @brief				Binds the socket to the given port on the given host name.
+		 * @param host			Host name.
+		 * @param port			Port number (as a string).
+		 */
+		void bind(const char* host, const char* port);
 
 	protected:
-		void				open();
-		addrinfo			hints() const noexcept;
+		/**
+		 * @brief				Opens the socket.
+		 */
+		void open();
 
-		void				tie(const char* host, const char* port, socket::tie_t tt);
-		void				tie(const socket::address& address, socket::tie_t tt);
+		/**
+		 * @brief				Returns the hints needed to obtain the host list.
+		 */
+		addrinfo hints() const noexcept;
+
+		/**
+		 * @brief				Binds or connects the socket to the given host and port.
+		 * @param host			Host name.
+		 * @param port			Port number (as a string).
+		 * @param tt			Bind or connect.
+		 */
+		void tie(const char* host, const char* port, socket::tie_t tt);
+
+		/**
+		 * @brief				Binds or connects the socket to the given address.
+		 * @param address		Address.
+		 * @param tt			Bind or connect.
+		 */
+		void tie(const socket::address& address, socket::tie_t tt);
 
 	private:
-		socket::error_t		tie(const sockaddr& addr, socklen_t addr_length, socket::tie_t tt);
+		/**
+		 * @brief				Binds or connects the socket to the given address. (Low level.)
+		 * @param addr			Low level address.
+		 * @param addr_length	Size of the low level address struct.
+		 * @param tt 
+		 * @return				`0` = success. `1` = error.
+		 */
+		socket::error_t tie(const sockaddr& addr, socklen_t addr_length, socket::tie_t tt);
 
 	protected:
-		const char*			any_host() const noexcept;
-		socket::kind_t		kind() const noexcept;
-		socket::family_t	family() const noexcept;
-		socket::protocol_t	protocol() const noexcept;
-		socket::handle_t	handle() const noexcept;
-		Log*				log() const noexcept;
+		/**
+		 * @brief				Returns a family-specific representation of any host.
+		 */
+		const char* any_host() const noexcept;
+
+		/**
+		 * @brief				Returns the socket kind - stream or datagram.
+		 */
+		socket::kind_t kind() const noexcept;
+
+		/**
+		 * @brief				Returns the socket family - IPv4 or IPv6.
+		 */
+		socket::family_t family() const noexcept;
+
+		/**
+		 * @brief				Returns the socket protocol - TCP or UDP.
+		 */
+		socket::protocol_t protocol() const noexcept;
+
+		/**
+		 * @brief				Returns the socket handle.
+		 */
+		socket::handle_t handle() const noexcept;
+
+		/**
+		 * @brief				Returns the Log pointer.
+		 */
+		Log* log() const noexcept;
 
 	private:
-		socket::kind_t		_kind;
-		socket::family_t	_family;
-		socket::protocol_t	_protocol;
-		socket::handle_t	_handle;
-		Log*				_log;
+		/**
+		 * @brief				The socket kind passed in to the constructor - stream or datagram.
+		 */
+		socket::kind_t _kind;
+
+		/**
+		 * @brief				The socket family passed in to the constructor - IPv4 or IPv6.
+		 */
+		socket::family_t _family;
+
+		/**
+		 * @brief				The socket protocol passed in to the constructor - TCP or UDP.
+		 */
+		socket::protocol_t _protocol;
+
+		/**
+		 * @brief				The socket handle.
+		 */
+		socket::handle_t _handle;
+
+		/**
+		 * @brief				The Log pointer passed in to the constructor.
+		 */
+		Log* _log;
 	};
 
 
 	// --------------------------------------------------------------
 
 
+	/**
+	 * @brief					Client (data transfer) socket functionality. Not directly constructable.
+	 * @tparam Log				Logging facility.
+	 */
 	template <typename Log>
 	class _client_socket : public _basic_socket<Log> {
 		using base = _basic_socket<Log>;
 
 	protected:
+		/**
+		 * @brief				Constructor.
+		 * @param kind			Stream or datagram.
+		 * @param family		IPv4 or IPv6.
+		 * @param log			Pointer to a `Log` instance. May be `nullptr`.
+		 */
 		_client_socket(socket::kind_t kind, socket::family_t family, Log* log);
+
+		/**
+		 * @brief				Constructor.
+		 * @param handle		Socket handle.
+		 * @param kind			Stream or datagram.
+		 * @param family		IPv4 or IPv6.
+		 * @param log			Pointer to a `Log` instance. May be `nullptr`.
+		 */
 		_client_socket(socket::handle_t handle, socket::kind_t kind, socket::family_t family, Log* log);
+
+		/**
+		 * @brief				Move constructor.
+		 */
 		_client_socket(_client_socket&& other) noexcept = default;
+
+		/**
+		 * @brief				Deleted.
+		 */
 		_client_socket(const _client_socket& other) = delete;
 
 	public:
+		/**
+		 * @brief				Connects the socket to the given port on the given host name. Optional for UDP sockets.
+		 * @param host			Host name.
+		 * @param port			Port number (as a string).
+		 */
 		void connect(const char* host, const char* port);
+
+		/**
+		 * @brief				Connects the socket to the given address. Optional for UDP sockets.
+		 * @param address		Address.
+		 */
 		void connect(const socket::address& address);
 
+		/**
+		 * @brief				Sends the bytes from the buffer into the socket.
+		 * @param buffer		Data buffer. 
+		 * @param size			Buffer size.
+		 * @param address		Remote address. Only needed for UDP sockets if `connect()` wasn't called.
+		 * @return				The number of bytes sent. `0` = error.
+		 */
 		std::size_t send(const void* buffer, std::size_t size, socket::address* address = nullptr);
+
+		/**
+		 * @brief				Receives the given number of bytes from the socket, and stores them into the buffer.
+		 * @param buffer		Data buffer. 
+		 * @param size			Buffer size.
+		 * @param address		Remote address. Only needed for UDP sockets if `connect()` wasn't called.
+		 * @return				The number of bytes received. `0` = error.
+		 */
 		std::size_t receive(void* buffer, std::size_t size, socket::address* address = nullptr);
 	};
 
@@ -166,14 +332,37 @@ namespace abc {
 	// --------------------------------------------------------------
 
 
+	/**
+	 * @brief					UDP socket functionality.
+	 * @tparam Log				Logging facility.
+	 */
 	template <typename Log = null_log>
 	class udp_socket : public _client_socket<Log> {
 		using base = _client_socket<Log>;
 
 	public:
+		/**
+		 * @brief				Constructor.
+		 * @param family		IPv4 or IPv6.
+		 * @param log			Pointer to a `Log` instance. May be `nullptr`.
+		 */
 		udp_socket(socket::family_t family = socket::family::ipv4, Log* log = nullptr);
+
+		/**
+		 * @brief				Constructor.
+		 * @details				IPv4 is assumed.
+		 * @param log			Pointer to a `Log` instance. May be `nullptr`.
+		 */
 		udp_socket(Log* log);
+
+		/**
+		 * @brief				Move constructor.
+		 */
 		udp_socket(udp_socket&& other) noexcept = default;
+
+		/**
+		 * @brief				Deleted.
+		 */
 		udp_socket(const udp_socket& other) = delete;
 	};
 
@@ -185,18 +374,48 @@ namespace abc {
 	class tcp_server_socket;
 
 
+	/**
+	 * @brief					TCP client socket functionality.
+	 * @tparam Log				Logging facility.
+	 */
 	template <typename Log = null_log>
 	class tcp_client_socket : public _client_socket<Log> {
 		using base = _client_socket<Log>;
 
 	public:
+		/**
+		 * @brief				Constructor.
+		 * @param family		IPv4 or IPv6.
+		 * @param log			Pointer to a `Log` instance. May be `nullptr`.
+		 */
 		tcp_client_socket(socket::family_t family = socket::family::ipv4, Log* log = nullptr);
+
+		/**
+		 * @brief				Constructor.
+		 * @details				IPv4 is assumed.
+		 * @param log			Pointer to a `Log` instance. May be `nullptr`.
+		 */
 		tcp_client_socket(Log* log);
+
+		/**
+		 * @brief				Move constructor.
+		 */
 		tcp_client_socket(tcp_client_socket&& other) noexcept = default;
+
+		/**
+		 * @brief				Deleted.
+		 */
 		tcp_client_socket(const tcp_client_socket& other) = delete;
 
 	protected:
 		friend tcp_server_socket<Log>;
+
+		/**
+		 * @brief				Internal constructor for accepted connections.
+		 * @param handle		Handle
+		 * @param family		IPv4 or IPv6.
+		 * @param log			Pointer to a `Log` instance. May be `nullptr`.
+		 */
 		tcp_client_socket(socket::handle_t handle, socket::family_t family, Log* log);
 	};
 
@@ -204,44 +423,124 @@ namespace abc {
 	// --------------------------------------------------------------
 
 
+	/**
+	 * @brief					TCP server socket functionality.
+	 * @tparam Log				Logging facility.
+	 */
 	template <typename Log = null_log>
 	class tcp_server_socket : public _basic_socket<Log> {
 		using base = _basic_socket<Log>;
 
 	public:
+		/**
+		 * @brief				Constructor.
+		 * @param family		IPv4 or IPv6.
+		 * @param log			Pointer to a `Log` instance. May be `nullptr`.
+		 */
 		tcp_server_socket(socket::family_t family = socket::family::ipv4, Log* log = nullptr);
+
+		/**
+		 * @brief				Constructor.
+		 * @details				IPv4 is assumed.
+		 * @param log			Pointer to a `Log` instance. May be `nullptr`.
+		 */
 		tcp_server_socket(Log* log);
+
+		/**
+		 * @brief				Move constructor.
+		 */
 		tcp_server_socket(tcp_server_socket&& other) noexcept = default;
+
+		/**
+		 * @brief				Deleted.
+		 */
 		tcp_server_socket(const tcp_server_socket& other) = delete;
 
 	public:
-		void					listen(socket::backlog_size_t backlog_size);
-		tcp_client_socket<Log>	accept() const;
+		/**
+		 * @brief				Starts listening.
+		 * @param backlog_size	Queue size.
+		 */
+		void listen(socket::backlog_size_t backlog_size);
+
+		/**
+		 * @brief				Blocks until a client tries to connect.
+		 * @return				New `tcp_client_socket` instance for the new connection.
+		 */
+		tcp_client_socket<Log> accept() const;
 	};
 
 
 	// --------------------------------------------------------------
 
 
+	/**
+	 * @brief					`std::streambuf` specialization that is backed by a socket.
+	 * @tparam Socket			`_client_socket`.
+	 * @tparam Log				Logging facility.
+	 */
 	template <typename Socket, typename Log = null_log>
 	class socket_streambuf : public std::streambuf {
 		using base = std::streambuf;
 
 	public:
+		/**
+		 * @brief				Constructor.
+		 * @param socket		Pointer to a `_client_socket` instance.
+		 * @param log			Pointer to a `Log` instance. May be `nullptr`.
+		 */
 		socket_streambuf(Socket* socket, Log* log = nullptr);
+
+		/**
+		 * @brief				Move constructor.
+		 */
 		socket_streambuf(socket_streambuf&& other) noexcept;
+
+		/**
+		 * @brief				Deleted.
+		 */
 		socket_streambuf(const socket_streambuf& other) = delete;
 
 	protected:
-		virtual int_type	underflow() override;
-		virtual int_type	overflow(int_type ch) override;
-		virtual int			sync() override;
+		/**
+		 * @brief				Handler that reads a byte from the socket.
+		 * @return				The byte received.
+		 */
+		virtual int_type underflow() override;
+
+		/**
+		 * @brief				Handler that sends a byte to the socket.
+		 * @param ch			Byte to be sent.
+		 * @return				`ch`
+		 */
+		virtual int_type overflow(int_type ch) override;
+
+		/**
+		 * @brief				Flushes.
+		 * @return				`0`
+		 */
+		virtual int sync() override;
 
 	private:
-		Socket*		_socket;
-		Log*		_log;
-		char		_get_ch;
-		char		_put_ch;
+		/**
+		 * @brief				The `_client_socket` pointer passed in to the constructor.
+		 */
+		Socket* _socket;
+
+		/**
+		 * @brief				The Log pointer passed in to the constructor.
+		 */
+		Log* _log;
+
+		/**
+		 * @brief				Cached char received.
+		 */
+		char _get_ch;
+
+		/**
+		 * @brief				Cached char to be sent.
+		 */
+		char _put_ch;
 	};
 
 }
