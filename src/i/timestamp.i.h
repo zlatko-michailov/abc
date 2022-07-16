c44c9d436e11cc030c79972f702c84c128fcd30c
Zlatko Michailov <zlatko@michailov.org>
2022-07-15 22:32:10 -0700

    doxygen: timestamp

--- src/i/timestamp.i.h
+++ src/i/timestamp.i.h
@@ -49,45 +49,166 @@ namespace abc {
 	// --------------------------------------------------------------
 
 
+	/**
+	 * @brief									Wrapper around `std::chrono::time_point` that has convenience getters for date and time parts.
+	 * @tparam Clock							Clock for the `std::chrono::time_point`.
+	 */
 	template <typename Clock = std::chrono::system_clock>
 	class timestamp : public std::chrono::time_point<Clock> {
 		using base = std::chrono::time_point<Clock>;
 
 	public:
+		/**
+		 * @brief								Constructor.
+		 * @details								Initializes with `0`.
+		 */
 		timestamp(std::nullptr_t) noexcept;
+
+		/**
+		 * @brief								Constructor.
+		 * @details								Initializes with the current time.
+		 */
 		timestamp() noexcept;
+
+		/**
+		 * @brief								Constructor.
+		 * @details								Initializes with a given time point.
+		 * @param tp							Time point.
+		 */
 		timestamp(const std::chrono::time_point<Clock>& tp) noexcept;
 
 	public:
+		/**
+		 * @brief								Resets with a given time point.
+		 * @param tp							Time point.
+		 */
 		void reset(std::chrono::time_point<Clock> tp) noexcept;
+
+		/**
+		 * @brief								Resets both the date and the time members with the given count of nanoseconds since epoch.
+		 * @param nanoseconds_since_epoch		Count of nanoseconds since epoch.
+		 */
 		void reset(time_count_t nanoseconds_since_epoch) noexcept;
 
+		/**
+		 * @brief								Resets the date members with the given count of days since epoch.
+		 * @param days_since_epoch				Count of days since epoch.
+		 */
 		void reset_date(date_count_t days_since_epoch) noexcept;
+
+		/**
+		 * @brief								Resets the time members with the given count of nanoseconds since midnight.
+		 * @param nanoseconds_since_midnight	Count of nanoseconds since midnight.
+		 */
 		void reset_time(time_count_t nanoseconds_since_midnight) noexcept;
 
 	public:
-		year_t			year()			const noexcept { return _year; }
-		month_t			month()			const noexcept { return _month; }
-		day_t			day()			const noexcept { return _day; }
+		/**
+		 * @brief								Returns the year part of the date.
+		 */
+		year_t year() const noexcept { return _year; }
 
-		hour_t			hours()			const noexcept { return _hours; }
-		minute_t		minutes()		const noexcept { return _minutes; }
-		second_t		seconds()		const noexcept { return _seconds; }
-		millisecond_t	milliseconds()	const noexcept { return _milliseconds; }
-		microsecond_t	microseconds()	const noexcept { return _microseconds; }
-		nanosecond_t	nanoseconds()	const noexcept { return _nanoseconds; }
+		/**
+		 * @brief								Returns the month part of the date.
+		 */
+		month_t month() const noexcept { return _month; }
+
+		/**
+		 * @brief								Returns the day part of the date.
+		 */
+		day_t day() const noexcept { return _day; }
+
+		/**
+		 * @brief								Returns the hours part of the time.
+		 */
+		hour_t hours() const noexcept { return _hours; }
+
+		/**
+		 * @brief								Returns the minutes part of the time.
+		 */
+		minute_t minutes() const noexcept { return _minutes; }
+
+		/**
+		 * @brief								Returns the seconds part of the time.
+		 */
+		second_t seconds() const noexcept { return _seconds; }
+
+		/**
+		 * @brief								Returns the milliseconds part of the time.
+		 */
+		millisecond_t milliseconds() const noexcept { return _milliseconds; }
+
+		/**
+		 * @brief								Returns the microseconds part of the time.
+		 */
+		microsecond_t microseconds() const noexcept { return _microseconds; }
+
+		/**
+		 * @brief								Returns the nanoseconds part of the time.
+		 */
+		nanosecond_t nanoseconds() const noexcept { return _nanoseconds; }
 
 	public:
+		/**
+		 * @brief								Operator `==`.
+		 */
 		bool operator==(const timestamp<Clock>& other) const noexcept;
+
+		/**
+		 * @brief								Operator `!=`.
+		 */
 		bool operator!=(const timestamp<Clock>& other) const noexcept;
+
+		/**
+		 * @brief								Operator `>`.
+		 */
 		bool operator> (const timestamp<Clock>& other) const noexcept;
+
+		/**
+		 * @brief								Operator `>=`.
+		 */
 		bool operator>=(const timestamp<Clock>& other) const noexcept;
+
+		/**
+		 * @brief								Operator `<`.
+		 */
 		bool operator< (const timestamp<Clock>& other) const noexcept;
+
+		/**
+		 * @brief								Operator `<=`.
+		 */
 		bool operator<=(const timestamp<Clock>& other) const noexcept;
 
 	private:
+		/**
+		 * @brief								Resets the date parts to the given values if the remaining days fall within a month. Otherwise, it moves to the next month.
+		 * @param days_since_epoch				Total days since epoch.
+		 * @param year							Year.
+		 * @param month							Month.
+		 * @param day							Day.
+		 * @param remaining_days				Remaining days.
+		 * @param days_in_1_month				The count of days in the month that is being considered.
+		 * @return								`true` = the date parts were reset. `false` = the process should continue.
+		 */
 		bool reset_date_if_done(date_count_t days_since_epoch, date_count_t& year, date_count_t& month, date_count_t& day, date_count_t& remaining_days, date_count_t days_in_1_month) noexcept;
+
+		/**
+		 * @brief								Unconditionally resets the date parts to the given values.
+		 * @param days_since_epoch				Total days since epoch.
+		 * @param year							Year.
+		 * @param month							Month.
+		 * @param day							Day.
+		 */
 		void reset_date(date_count_t days_since_epoch, year_t year, month_t month, day_t day) noexcept;
+
+		/**
+		 * @brief								Unconditionally resets the time parts to the given values.
+		 * @param nanoseconds_since_midnight	Nanoseconds since midnight.
+		 * @param hours							Hours.
+		 * @param minutes						Minutes.
+		 * @param seconds						Seconds.
+		 * @param nanoseconds					Nanoseconds.
+		 */
 		void reset_time(time_count_t nanoseconds_since_midnight, hour_t hours, minute_t minutes, second_t seconds, nanosecond_t nanoseconds) noexcept;
 
 	private:
