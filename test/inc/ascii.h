86c41cf6e64902de6a1abfa0fdf5715e9d994414
Zlatko Michailov <zlatko@michailov.org>
2023-08-29 23:10:27 -0700

    ascii: Enable tests

--- test/inc/ascii.h
+++ test/inc/ascii.h
@@ -30,12 +30,7 @@ SOFTWARE.
 #include "test.h"
 
 
-namespace abc { namespace test { namespace ascii {
-
-    bool test_ascii_equal(test_context<abc::test::log>& context);
-    bool test_ascii_equal_n(test_context<abc::test::log>& context);
-    bool test_ascii_equal_i(test_context<abc::test::log>& context);
-    bool test_ascii_equal_i_n(test_context<abc::test::log>& context);
-
-}}}
-
+bool test_ascii_equal(test_context& context);
+bool test_ascii_equal_n(test_context& context);
+bool test_ascii_equal_i(test_context& context);
+bool test_ascii_equal_i_n(test_context& context);
