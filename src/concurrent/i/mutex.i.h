1fb41487054ccf90c937bc466d4b03a4989a7d51
Zlatko Michailov <zlatko@michailov.org>
2025-11-13 20:10:17 -0800

    mutex: Drafted

---
+++ src/concurrent/i/mutex.i.h
@@ -0,0 +1,120 @@
+/*
+MIT License
+
+Copyright (c) 2018-2025 Zlatko Michailov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+#pragma once
+
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+#include "../../diag/i/diag_ready.i.h"
+
+
+namespace abc { namespace concurrent {
+
+    /**
+     * @brief Mutex that exposes the ID of the thread that has locked it. 
+     */
+    class mutex
+        : protected diag::diag_ready<const char*> {
+
+        using diag_base = diag::diag_ready<const char*>;
+
+    public:
+        /**
+         * @brief     Constructor.
+         * @param log `diag::log_ostream` pointer. May be `nullptr`.
+         */
+        mutex(diag::log_ostream* log = nullptr);
+
+        /**
+         * @brief Deleted.
+         */
+        mutex(mutex&& other) noexcept = delete;
+
+        /**
+         * @brief Deleted.
+         */
+        mutex(const mutex& other) = delete;
+
+    public:
+        /**
+         * @brief Locks the mutex.
+         */
+        void lock();
+
+        /**
+         * @brief  Tries to lock the mutex. Returns immediately.
+         * @return `true` = a lock was acquired. `false` = a lock was not acquired. 
+         */
+        bool try_lock();
+
+        /**
+         * @brief Unlocks the mutex.
+         */
+        void unlock();
+
+    public:
+        /**
+         * @brief Returns `true` if the mutex is currently locked, `false` otherwise.
+         */
+        bool is_locked() noexcept;
+
+        /**
+         * @brief Returns the ID of the thread that has locked the mutex.
+         */
+        std::thread::id get_locking_thread_id() noexcept;
+
+        /**
+         * @brief Returns `true` if the mutex is currently locked by the current thread, `false` otherwise.
+         */
+        operator bool () noexcept;
+
+    private:
+        /**
+         * @brief Flag that indicates whether the mutex is currently locked.
+         */
+        bool _is_locked;
+
+        /**
+         * @brief The ID of the thread that has locked the mutex.
+         */
+        std::thread::id _thread_id;
+
+        /**
+         * @brief Mutex to protect the internal state.
+         */
+        std::mutex _state_mutex;
+
+        /**
+         * @brief Condition variable to block on lock attempts.
+         */
+        std::condition_variable_any _blocker;
+    };
+
+
+    // --------------------------------------------------------------
+
+} }
---
+++ src/concurrent/mutex.h
@@ -0,0 +1,137 @@
+/*
+MIT License
+
+Copyright (c) 2018-2025 Zlatko Michailov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+#pragma once
+
+#include "../diag/diag_ready.h"
+#include "i/mutex.i.h"
+
+
+namespace abc { namespace concurrent {
+
+    mutex::mutex(diag::log_ostream* log)
+        : diag_base("abc::concurrent::mutex", log)
+        , _is_locked(false)
+        , _thread_id() {
+
+        constexpr const char* suborigin = "mutex()";
+        diag_base::put_any(suborigin, diag::severity::callstack, __TAG__, "Begin:");
+
+        diag_base::put_any(suborigin, diag::severity::callstack, __TAG__, "End:");
+    }
+
+
+    void mutex::lock() {
+        constexpr const char* suborigin = "lock()";
+        diag_base::put_any(suborigin, diag::severity::callstack, __TAG__, "Begin:");
+
+        {
+            std::lock_guard<std::mutex> state_lock(_state_mutex);
+
+            std::thread::id this_thread_id = std::this_thread::get_id();
+
+            if (_is_locked) {
+                diag_base::expect(suborigin, _thread_id != this_thread_id, __TAG__, "_thread_id != this_thread_id");
+
+                _blocker.wait(state_lock, [=]{ return !_is_locked; });
+            }
+
+            _is_locked = true;
+            _thread_id = this_thread_id;
+        }
+
+        diag_base::put_any(suborigin, diag::severity::callstack, __TAG__, "End:");
+    }
+
+
+    bool mutex::try_lock() {
+        constexpr const char* suborigin = "try_lock()";
+        diag_base::put_any(suborigin, diag::severity::callstack, __TAG__, "Begin:");
+
+        bool ret = false;
+        {
+            std::lock_guard<std::mutex> state_lock(_state_mutex);
+
+            std::thread::id this_thread_id = std::this_thread::get_id();
+
+            if (!_is_locked) {
+                _is_locked = true;
+                _thread_id = this_thread_id;
+                ret = true;
+            }
+        }
+
+        diag_base::put_any(suborigin, diag::severity::callstack, __TAG__, "End: ret=%d", (int)(ret));
+
+        return ret;
+    }
+
+
+    void mutex::unlock() {
+        constexpr const char* suborigin = "unlock()";
+        diag_base::put_any(suborigin, diag::severity::callstack, __TAG__, "Begin:");
+
+        {
+            std::lock_guard<std::mutex> state_lock(_state_mutex);
+
+            std::thread::id this_thread_id = std::this_thread::get_id();
+
+            diag_base::expect(suborigin, _is_locked, __TAG__, "_is_locked");
+            diag_base::expect(suborigin, _thread_id == this_thread_id, __TAG__, "_thread_id == this_thread_id");
+
+            _is_locked = false;
+            _thread_id = std::thread::id();
+        }
+
+        _blocker.notify_one();
+
+        diag_base::put_any(suborigin, diag::severity::callstack, __TAG__, "End:");
+    }
+
+
+    bool mutex::is_locked() noexcept {
+        std::lock_guard<std::mutex> lock(_state_mutex);
+
+        return _is_locked;
+    }
+
+ 
+    std::thread::id mutex::get_locking_thread_id() noexcept {
+        std::lock_guard<std::mutex> lock(_state_mutex);
+
+        return _thread_id;
+    }
+
+
+    mutex::operator bool () noexcept {
+        std::lock_guard<std::mutex> lock(_state_mutex);
+
+        return _is_locked && (_thread_id == std::this_thread::get_id());
+    }
+
+
+    // --------------------------------------------------------------
+
+} }
